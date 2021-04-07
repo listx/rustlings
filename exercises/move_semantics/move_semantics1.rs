2fba7285adacba26a238d2da30cb41f797ab7ea7
Linus Arver <linusa@google.com>
2021-04-06 23:08:59 -0700

    move_semantics1

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -1,12 +1,15 @@
 // move_semantics1.rs
 // Make me compile! Execute `rustlings hint move_semantics1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
-    let vec1 = fill_vec(vec0);
+    // We have to declare vec1 to be a mutable reference, because we (the main()
+    // function) are going to modify it below with `vec1.push(88)`. The .push()
+    // method (https://doc.rust-lang.org/std/vec/struct.Vec.html#method.push)
+    // stipulates that it only takes a mutable reference. So trying to use an
+    // immutable reference with it is a natural compiler error.
+    let mut vec1 = fill_vec(vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
