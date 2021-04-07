5cd374ea014394ca03041945d292f25108dc64d4
Linus Arver <linusa@google.com>
2021-04-06 22:55:59 -0700

    functions5

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -1,13 +1,15 @@
 // functions5.rs
 // Make me compile! Execute `rustlings hint functions5` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let answer = square(3);
     println!("The answer is {}", answer);
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    // `num * num` is an expression.
+    num * num
+    // Alternative: `return num * num;`, which is a statement. Statements
+    // (denoted by a semicolon) do not evaluate to a value by themselves, so if
+    // we want the value out of it, we have to use the `return` keyword.
 }
