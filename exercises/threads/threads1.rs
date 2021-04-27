bcb80bb347fb524f0a17b5ce62ef974a297c28ef
Linus Arver <linusa@google.com>
2021-04-27 15:52:08 -0700

    threads1

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -6,9 +6,9 @@
 // of "waiting..." and the program ends without timing out when running,
 // you've got it :)
 
-// I AM NOT DONE
-
-use std::sync::Arc;
+// Mutex gives us the lock() method which lets us acquire a region of memory
+// while denying it from other threads.
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -17,16 +17,48 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    // We use a Mutex to protect shared data. The "status" and "shared_status"
+    // variables both point to the same region of memory, so we use a Mutex to
+    // protect it. This also allows us to mutate that region of memory (mutation
+    // is implied, and is why we don't have to declare a mutable reference from
+    // inside the spawned thread below); see
+    // https://doc.rust-lang.org/std/sync/struct.Arc.html, which states:
+    //
+    //     If you need to mutate through an Arc, use Mutex, RwLock, or one of
+    //     the Atomic types.
+    //
+    // This thought is echoed again in
+    // https://doc.rust-lang.org/book/ch16-03-shared-state.html#similarities-between-refcelltrct-and-mutextarct,
+    // which says:
+    //
+    //     ...this means Mutex<T> provides interior mutability, as the Cell
+    //     family does. In the same way we used RefCell<T> in Chapter 15 to allow us
+    //     to mutate contents inside an Rc<T>, we use Mutex<T> to mutate contents
+    //     inside an Arc<T>.
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
+    // "status_shared" refers to the *SAME* region of memory as "status". See
+    // https://doc.rust-lang.org/std/sync/struct.Arc.html#cloning-references.
     let status_shared = status.clone();
     thread::spawn(move || {
         for _ in 0..10 {
-            thread::sleep(Duration::from_millis(250));
-            status_shared.jobs_completed += 1;
+            thread::sleep(Duration::from_millis(25));
+            // Acquire the region of memory from other threads (in this case,
+            // the main thread). If we can't acquire it, we panic().
+            status_shared.lock().unwrap().jobs_completed += 1;
         }
     });
-    while status.jobs_completed < 10 {
+
+    while status.lock().unwrap().jobs_completed < 10 {
         println!("waiting... ");
-        thread::sleep(Duration::from_millis(500));
+        thread::sleep(Duration::from_millis(50));
     }
+
+    // Create a mutable reference to JobStatus (just for fun). We can mutate
+    // freely now because the spawned thread above has terminated.
+    let mut x = status.lock().unwrap();
+    println!("{}", x.jobs_completed);
+    x.jobs_completed += 1;
+    println!("{}", x.jobs_completed);
+    x.jobs_completed += 1;
+    println!("{}", x.jobs_completed);
 }
