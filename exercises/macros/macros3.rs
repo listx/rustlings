97b1ccc443ae565d7a89f6138df737ab92cc2be9
Linus Arver <linusa@google.com>
2021-04-27 16:28:51 -0700

    macros3

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -2,8 +2,9 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` for hints :)
 
-// I AM NOT DONE
-
+// See https://danielkeep.github.io/tlborm/book/mbe-min-import-export.html for
+// info about #[macro_use].
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
