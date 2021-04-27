b4d5ac5d019a3ab85550b15e050a735836ace7f7
Linus Arver <linusa@google.com>
2021-04-27 16:27:12 -0700

    macros2

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -1,12 +1,17 @@
 // macros2.rs
 // Make me compile! Execute `rustlings hint macros2` for hints :)
 
-// I AM NOT DONE
+// See https://danielkeep.github.io/tlborm/book/mbe-min-import-export.html for
+// #[macro_export]. Also see
+// https://danielkeep.github.io/tlborm/book/mbe-min-scoping.html for a
+// discussion of scoping rules (including the ordering of macro definitions vs.
+// their usage, which matters, unlike regular functions).
 
 fn main() {
     my_macro!();
 }
 
+#[macro_export]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
