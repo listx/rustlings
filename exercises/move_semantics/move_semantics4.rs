a1dd2a20fefb44e086da70691704a7da8ef9b52d
Linus Arver <linusa@google.com>
2021-04-28 11:52:04 -0700

    move_semantics4: answer

--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -7,9 +7,7 @@
 // I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec();
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -21,8 +19,14 @@ fn main() {
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument
 //
 // Q: How can we make fill_vec() create a new vector from scratch?
+//
+// A: We just move the vector creation (Vec::new()) from main() to be
+// within fill_vec()'s definition.
+//
+// This is exactly like Vec::new() itself, but with a twist that it just comes
+// pre-allocated with some initial hardcoded elements.
 fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec = Vec::new();
 
     vec.push(22);
     vec.push(44);
