a22d4ec0f01f0bc6b33c1dd09409f44701927819
Linus Arver <linusa@google.com>
2021-04-28 10:36:24 -0700

    move_semantics1: extended problem statement

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -4,8 +4,28 @@
 // I AM NOT DONE
 
 fn main() {
+    // Here vec0 is an immutable reference to the heap-allocated vector
+    // (basically a dynamic array). From here on out, vec0 cannot be used to
+    // modify the underlying memory in the vector (even though the memory is
+    // heap-allocated and totally supports growing and shrinking based on
+    // runtime usage). Anyway, making vec0 immutable here is OK because we don't
+    // use vec0 from within this scope to modify it. So, someone else is free to
+    // grow the vector, just not us.
     let vec0 = Vec::new();
 
+    // Indeed, we just pass along this immutable reference to fill_vec(), which
+    // can grow it. Even though fill_vec() will grow the vector, it's OK because
+    // fill_vec() is the one doing the growing, NOT main().
+    //
+    // When fill_vec() is done it returns the same memory location back to us
+    // (the same region of memory pointed-to by vec0).
+    //
+    // When we create vec1, we're saying that we still want to make it
+    // immutable. There's nothing wrong with this per-se, it's just that we
+    // break this contract by trying to modify it below with vec1.push(88).
+    //
+    // Q: how can we declare a different intent here so that we can mutate
+    // the underlying vector within main() directly?
     let vec1 = fill_vec(vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
@@ -15,6 +35,7 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
+// Take a vector, then grow it with 3 more elements.
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
