fda0ff076bd75034431892fc4cd70ad278c0c673
Linus Arver <linusa@google.com>
2021-04-28 11:35:23 -0700

    move_semantics3: extended problem statement

--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -17,6 +17,14 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
+// Here we are quite simply modifying the underlying memory pointed by vec,
+// by calling push(). As it stands though, the declaration in the parameter
+// list "vec: Vec<i32>" states that "vec" is an immutable variable. And so
+// either we have to perform only non-mutating (read-only) operations on it,
+// or, if we want to keep the push() calls to modify it, declare that we
+// expect it to be mutable instead.
+//
+// Q: How do we make "vec" mutable?
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     vec.push(22);
     vec.push(44);
